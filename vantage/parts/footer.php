0bd818314bc75c1f033d1370d91f3aa64704187a
Lov Singh <lov.singh@live.com>
2014-07-03 00:07:19 +0530

    Update footer.php

--- vantage/parts/footer.php
+++ vantage/parts/footer.php
@@ -9,7 +9,7 @@
 		<?php dynamic_sidebar( 'sidebar-footer' ) ?>
 	</div><!-- #footer-widgets -->
 
-	<?php $site_info_text = apply_filters('vantage_site_info', siteorigin_setting('general_site_info_text') ); if( !empty($site_info_text) ) : ?>
+	
 		<div id="site-info">
 			<?php echo wp_kses_post($site_info_text) ?>
 		</div><!-- .site-info -->
